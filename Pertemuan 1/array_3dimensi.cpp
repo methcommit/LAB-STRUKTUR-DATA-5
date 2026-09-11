1860cfaf9d6b2746404aa937f42cd565065ef6cf
anakin.... why? <mariaslinggk@gmail.com>
2026-09-11 10:15:10 +0700

    Add files via upload

---
+++ Pertemuan 1/array_3dimensi.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    system("cls");
+
+    int x, y, z;
+
+    cout << "Masukkan jumlah baris: ";
+    cin >> x;
+    cout << "Masukkan jumlah kolom: ";
+    cin >> y;
+    cout << "Masukkan jumlah kedalaman: ";
+    cin >> z;
+    
+}
